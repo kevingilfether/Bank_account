c1932d7b3e358b985847ba725914025175be1f63
kevingilfether <kevingilfether@gmail.com>
2017-10-13 11:29:38 -0400

    Finished Client class

--- Bank_account/Checking.cs
+++ Bank_account/Checking.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 namespace Bank_account
 {
     class Checking: Account
-    {
+    { 
+        //This is the field for 
     }
 }
--- Bank_account/Client.cs
+++ Bank_account/Client.cs
@@ -8,5 +8,40 @@ namespace Bank_account
 {
     class Client
     {
+        //This is the class representing a client at the bank
+
+        private string name;
+        private string userName;
+        private int acctNum;
+
+        public string Name
+        {
+            get { return this.name; }
+        }
+        public string UserName
+        {
+            get { return this.userName; }
+        }
+        public int AcctNum
+        {
+            get { return this.acctNum; }
+        }
+
+        public Client()
+        {
+
+        }
+
+        public Client(string name, string userName, int acctNum)
+        {
+            this.name = name;
+            this.userName = userName;
+            this.acctNum = acctNum;
+        }
+        public void ViewClientInfo()
+        {
+            Console.WriteLine(Name + " --- " + userName + " --- " + acctNum);
+        }
+
     }
 }
